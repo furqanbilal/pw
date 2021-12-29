bb56f9988c9e0975d005b053db04c83422294aa3
furqanbilal <furquanbazaz@gmail.com>
2021-12-29 22:37:11 +0530

    all the basic logic added for password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+
+  length=0;
+  isLength=false;
+  useLetters=false;
+  useNumbers=false;
+  useSymbols=false;
+  password='';
+
+  onChangeLength(element:any){
+    const parsedValue=parseInt(element.target.value);
+
+    if(parsedValue!=NaN){
+      length=parsedValue;
+    }
+
+    if (length!=0)
+      this.isLength=true;
+    else
+      this.isLength=false;
+  }
+
+  onChangeUseLetters(){
+    this.useLetters =!this.useLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.useNumbers =!this.useNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onButtonClick(){
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    const numbers='1234567890';
+    const symbols='!@#$%^&*()_+';
+
+    let validChars = '';
+
+    if(this.useLetters){
+      validChars += letters;
+    }
+
+    if(this.useNumbers){
+      validChars += numbers;
+    }
+
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword='';
+
+    for (let i=0; i < length; i++){
+      const index = Math.floor(Math.random()*validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
 }
